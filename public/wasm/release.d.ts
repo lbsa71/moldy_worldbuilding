262d6470237222387f67dcfb262d18fde8374cb0
Stefan Andersson <stefan.andersson@hiberworld.com>
2024-11-21 14:47:50 +0100

    wip

--- public/wasm/release.d.ts
+++ public/wasm/release.d.ts
@@ -1,10 +1,15 @@
-/** Exported memory */
-export declare const memory: WebAssembly.Memory;
-/**
- * assembly/gameLogic/calculatePhysics
- * @param x `f32`
- * @param y `f32`
- * @param z `f32`
- * @returns `f32`
- */
-export declare function calculatePhysics(x: number, y: number, z: number): number;
+declare namespace __AdaptedExports {
+  /** Exported memory */
+  export const memory: WebAssembly.Memory;
+  /**
+   * assembly/gameLogic/calculatePhysics
+   * @param x `f32`
+   * @param y `f32`
+   * @param z `f32`
+   * @returns `f32`
+   */
+  export function calculatePhysics(x: number, y: number, z: number): number;
+}
+/** Instantiates the compiled WebAssembly module with the given imports. */
+export declare function instantiate(module: WebAssembly.Module, imports: {
+}): Promise<typeof __AdaptedExports>;
